713652fb102cfc1418ced9e654666802c5dfebcf
Pawel Zawistowski <pawel.zawistowski@allegro.pl>
2015-02-13 08:38:20 +0100

    Changed API to utilise proto package.

--- R/caen.R
+++ R/caen.R
@@ -1,34 +1,28 @@
+library(proto)
+
 nvl <- function(v1,v2){
   if(is.null(v1)) v2 else v1  
 }
 
 
-encodeColumn <- function(atts, map){
-  
-  vapply(atts, FUN=function(v) nvl(map[[v]], map$default), FUN.VALUE=0.0, USE.NAMES = FALSE)
+encodeColumn <- function(atts, map, default){
+  vapply(atts, FUN=function(v) nvl(map[[v]], default), FUN.VALUE=0.0, USE.NAMES = FALSE)
 }
 
-calculateMap <- function(atts, target){
+#' @export
+CaEn <- function(atts, target){
   tab <- table(atts, target)
   
   map <- as.list((tab/rowSums(tab))[,1])
   names(map) <- rownames(tab)
   
-  map$default <- sum(tab[,1])/sum(colSums(tab))
+  default <- sum(tab[,1])/sum(colSums(tab))
+  map <- map
   
-  map
-}
-
-#' @export
-encode <- function(atts, target = NULL, map = NULL, map.return = FALSE){
-  if(is.null(map)){
-    map <- calculateMap(atts, target)
-  }
-  
-  encoded = encodeColumn(atts, map) 
-  if(map.return){
-    list( encoded = encoded, map = map)
-  }else{
-    encoded
-  }
+  proto(map = map, 
+        default = default,
+        encode =  function(this, atts){
+          encodeColumn(atts, this$map, this$default)   
+        }
+  )
 }
